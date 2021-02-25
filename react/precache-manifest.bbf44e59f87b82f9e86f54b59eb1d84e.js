12f41d026c04eca0424b95f8215939f47f5f3d13
stnc <stnc@stncs-iMac.local>
2021-02-26 00:58:41 +0300

    puplish

---
+++ react/precache-manifest.bbf44e59f87b82f9e86f54b59eb1d84e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058c7250504e4bff920d2a4cd65aae4f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "f754c70cff42fed38106",
+    "url": "/react/static/css/main.2604931f.chunk.css"
+  },
+  {
+    "revision": "b59c1abb7e5e952c855b",
+    "url": "/react/static/js/2.cb012b7c.chunk.js"
+  },
+  {
+    "revision": "d84af5889e5bd1350816742e9cfd5e3d",
+    "url": "/react/static/js/2.cb012b7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f754c70cff42fed38106",
+    "url": "/react/static/js/main.9d561a6d.chunk.js"
+  },
+  {
+    "revision": "4cf96134d0cc5cf261e8",
+    "url": "/react/static/js/runtime-main.62e0ceda.js"
+  }
+]);
